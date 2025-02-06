9fe3dc44cd0a75ee61b844ac9bba5d1e2d6427f3
Noah Carlos <ndcarlos415@gmail.com>
2025-02-05 23:23:00 -0800

    creating input fomrs and start connection to wander.py

---
+++ wander/wander_django/wander/views.py
@@ -0,0 +1,27 @@
+# Create your views here.
+import requests
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'wander')))
+from Spotify_10.24 import wander
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+
+# Landing and form processing page
+def index(request):
+	if request.method == "POST":
+		#Extract the data from the form
+		genre = request.POST.get('genre')
+		track_count = request.POST.get('track_count')
+
+		#Process data through wander.py
+		result = wander.wander.get_artists_by_genre(genre)
+
+
+		return request, 'index.html', {'result:'}
+	
+	return render(request, 'wander/index.html')
+
+
